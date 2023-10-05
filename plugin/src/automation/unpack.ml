8fe0d925f4df529d064539c4881318c2b57530b0
Arpan Agrawal <arpan2@illinois.edu>
2023-10-05 14:51:59 +0530

    Use the tactic string for name.

--- plugin/src/automation/unpack.ml
+++ plugin/src/automation/unpack.ml
@@ -10,12 +10,12 @@ let eval_tactic env sigma ?goal tac =
   let (sigma, (typ, _)) = Evarutil.new_type_evar env sigma Evd.univ_flexible_alg in
   let (ent, pv) = Proofview.init sigma [(env, typ)] in
   let sigma0 = sigma in
-  let ((), pv, (unsafe, shelved, obliged), _) = Proofview.apply ~name:(Names.Id.of_string "placeholder") ~poly:true env tac pv in
-  let sigma = Proofview.proofview pv |> snd in
+  let ((), pv, (unsafe, shelved, obliged), _) = Proofview.apply ~name:(qualid_basename tactic_script) ~poly:true env tac pv in
+  let sigma = Proofview.return pv in
   (* NOTE: Technically our current examples/tests do not require this post-processing
    * unification step, but I suspect that it may sometimes be necessary to ensure that
    * Coq handles any lingering typeclass/implicit argument inference in the usual way. *)
-  let sigma = Pretyping.solve_remaining_evars (Pretyping.default_inference_flags true) env sigma ~initial:sigma0 in
+  let sigma = Pretyping.solve_remaining_evars (Pretyping.default_inference_flags true) env ~initial:sigma0 sigma  in
   let proofs = Proofview.partial_proof ent pv |> List.map (EConstr.to_constr sigma) in
   List.hd proofs
 
